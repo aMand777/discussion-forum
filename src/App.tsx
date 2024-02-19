fd4508e0f928c94f26b0f4ce40380fab921082ba
aMand7 <amanddseventaurus@gmail.com>
2024-02-19 14:30:31 +0700

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello React</h1>
+      <button className="btn btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
