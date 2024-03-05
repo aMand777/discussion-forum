c5cc5b788ca12da61f2eeae53b88a809b6d83144
aMand7 <amanddseventaurus@gmail.com>
2024-03-06 00:29:02 +0700

    feat: Mengganti style tampilan input pada compnent form login dan register

--- src/components/auth/login/FormLogin.tsx
+++ src/components/auth/login/FormLogin.tsx
@@ -32,38 +32,57 @@ const FormLogin: React.FC<FormLoginProps> = ({ register, errors, onSubmit, handl
         </div>
         <p className='text-2xl font-semibold'>Login</p>
         <form onSubmit={handleSubmit(onSubmit)} className='w-11/12 md:max-w-md' noValidate>
-          <label className='w-full form-control'>
-            <div className='label'>
-              <span className='label-text'>Email</span>
-            </div>
+          <div className='label'>
+            <span className='label-text'>Email</span>
+          </div>
+          <label className='input input-bordered flex items-center gap-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+              fill='currentColor'
+              className='w-4 h-4 opacity-70'>
+              <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
+              <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
+            </svg>
             <input
-              type='email'
+              type='text'
+              className='grow'
               placeholder='example@mail.com'
-              className={`${errors.email ? 'input-error' : ''} input input-bordered w-full`}
               {...register('email')}
             />
-            {errors.email ? (
-              <AlertMessage message={errors.email?.message} />
-            ) : (
-              message?.toLowerCase().includes('email') && <AlertMessage message={message} />
-            )}
           </label>
-          <label className='w-full form-control'>
-            <div className='label'>
-              <span className='label-text'>Password</span>
-            </div>
+          {errors.email ? (
+            <AlertMessage message={errors.email?.message} />
+          ) : (
+            message?.toLowerCase().includes('email') && <AlertMessage message={message} />
+          )}
+          <div className='label'>
+            <span className='label-text'>Password</span>
+          </div>
+          <label className='input input-bordered flex items-center gap-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+              fill='currentColor'
+              className='w-4 h-4 opacity-70'>
+              <path
+                fillRule='evenodd'
+                d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
+                clipRule='evenodd'
+              />
+            </svg>
             <input
               type={showPassword ? 'text' : 'password'}
+              className='grow'
               placeholder='Password*'
-              className={`${errors.password ? 'input-error' : ''} input input-bordered w-full`}
               {...register('password')}
             />
-            {errors.password ? (
-              <AlertMessage message={errors.password?.message} />
-            ) : (
-              message?.toLowerCase().includes('password') && <AlertMessage message={message} />
-            )}
           </label>
+          {errors.password ? (
+            <AlertMessage message={errors.password?.message} />
+          ) : (
+            message?.toLowerCase().includes('password') && <AlertMessage message={message} />
+          )}
           <label className='cursor-pointer label'>
             <span className='label-text'>Show Password</span>
             <input
--- src/components/auth/register/FormRegister.tsx
+++ src/components/auth/register/FormRegister.tsx
@@ -37,66 +37,77 @@ const FormRegister: React.FC<FormRegisterProps> = ({
     <div className='w-full flex justify-center items-center mt-10'>
       <div className='flex flex-col justify-center items-center container'>
         <div className='bg-secondary rounded-full p-1 text-secondary-content mb-2'>
-          <img src='/forum.png' alt='forum-icon' className='w-16'/>
+          <img src='/forum.png' alt='forum-icon' className='w-16' />
         </div>
         <p className='text-2xl font-semibold'>Register</p>
         <form onSubmit={handleSubmit(onSubmit)} className='w-11/12 md:max-w-md' noValidate>
-          <label className='form-control w-full'>
-            <div className='label'>
-              <span className='label-text'>Name</span>
-            </div>
-            <input
-              type='name'
-              placeholder='example@mail.com'
-              className={`${errors.name ? 'input-error' : ''} input input-bordered w-full`}
-              {...register('name')}
-            />
-            {errors.name && <AlertMessage message={errors.name.message} />}
+          <label className='input input-bordered flex items-center gap-2 mt-5'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+              fill='currentColor'
+              className='w-4 h-4 opacity-70'>
+              <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z' />
+            </svg>
+            <input type='text' className='grow' placeholder='Username' {...register('name')} />
           </label>
-          <label className='form-control w-full'>
-            <div className='label'>
-              <span className='label-text'>Email</span>
-            </div>
-            <input
-              type='email'
-              placeholder='example@mail.com'
-              className={`${errors.email ? 'input-error' : ''} input input-bordered w-full`}
-              {...register('email')}
-            />
-            {errors.email ? (
-              <AlertMessage message={errors.email?.message} />
-            ) : (
-              message?.toLowerCase().includes('email') && (
-                <AlertMessage message={message} />
-              )
-            )}
+          {errors.name && <AlertMessage message={errors.name.message} />}
+          <label className='input input-bordered flex items-center gap-2 mt-5'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+              fill='currentColor'
+              className='w-4 h-4 opacity-70'>
+              <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
+              <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
+            </svg>
+            <input type='text' className='grow' placeholder='Email' {...register('email')} />
           </label>
-          <label className='form-control w-full'>
-            <div className='label'>
-              <span className='label-text'>Password</span>
-            </div>
+          {errors.email ? (
+            <AlertMessage message={errors.email?.message} />
+          ) : (
+            message?.toLowerCase().includes('email') && <AlertMessage message={message} />
+          )}
+          <label className='input input-bordered flex items-center gap-2 mt-5'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+              fill='currentColor'
+              className='w-4 h-4 opacity-70'>
+              <path
+                fillRule='evenodd'
+                d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
+                clipRule='evenodd'
+              />
+            </svg>
             <input
               type={showPassword ? 'text' : 'password'}
+              className='grow'
               placeholder='Password*'
-              className={`${errors.password ? 'input-error' : ''} input input-bordered w-full`}
               {...register('password')}
             />
-            {errors.password && <AlertMessage message={errors.password.message} />}
           </label>
-          <label className='form-control w-full'>
-            <div className='label'>
-              <span className='label-text'>Confirm Password</span>
-            </div>
+          {errors.password && <AlertMessage message={errors.password.message} />}
+          <label className='input input-bordered flex items-center gap-2 mt-5'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+              fill='currentColor'
+              className='w-4 h-4 opacity-70'>
+              <path
+                fillRule='evenodd'
+                d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
+                clipRule='evenodd'
+              />
+            </svg>
             <input
               type={showPassword ? 'text' : 'password'}
+              className='grow'
               placeholder='Confirm Password*'
-              className={`${
-                errors.confirmPassword ? 'input-error' : ''
-              } input input-bordered w-full`}
               {...register('confirmPassword')}
             />
-            {errors.confirmPassword && <AlertMessage message={errors.confirmPassword.message} />}
           </label>
+          {errors.confirmPassword && <AlertMessage message={errors.confirmPassword.message} />}
           <label className='label cursor-pointer'>
             <span className='label-text'>Show Password</span>
             <input
