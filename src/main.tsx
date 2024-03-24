6ad1d5c21d655e9fc97232ea88b743ed5f76fc59
aMand7 <amanddseventaurus@gmail.com>
2024-03-25 00:47:06 +0700

    add package @tanstack/react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'next-themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import store from './states/store.ts';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <ThemeProvider attribute="data-theme">
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </ThemeProvider>
       </Provider>
     </BrowserRouter>
